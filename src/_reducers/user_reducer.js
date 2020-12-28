2e07645069fb0cbae712d45d40516d51407d2dbe
이소정 <sjlee950326@gmail.com>
2020-12-28 14:52:35 +0900

    4 - 디렉토리 세팅 / 리액트와 리덕스 디렉토리 기본 세팅, 디렉토리 구조 잡기

--- src/_reducers/user_reducer.js
+++ src/_reducers/user_reducer.js
@@ -1,4 +1,4 @@
-import { USER_SIGNIN, USER_SIGNUP, USER_SIGNOUT } from '../_actions/user_action';
+import { USER_SIGNIN, USER_SIGNUP, USER_SIGNOUT } from '../_actions/types';
 
 const initialState = {};
 
